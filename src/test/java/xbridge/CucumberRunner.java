c2bfc9cad692c2f25f3e6deb5d3e45e63aeed58e
ngsmrk <ngsmrk@gmail.com>
2012-02-27 11:00:43 +0000

    Added exclusions

--- src/test/java/xbridge/CucumberRunner.java
+++ src/test/java/xbridge/CucumberRunner.java
@@ -2,9 +2,29 @@ package xbridge;
 
 import cucumber.cli.Main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class CucumberRunner {
 
     public static void main(String[] argv) throws Throwable {
-        Main.main(argv);
+
+        final List<String> args = new ArrayList<String>(asList(argv));
+
+        String rerunFileLocation;
+        for (int i = 0; i < args.size(); i++) {
+            String arg = args.get(i);
+            if ("--rerun".equals(arg)) {
+                rerunFileLocation = args.get(i + 1);
+            }
+        }
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+        Main.main(args.toArray(new String[]{}));
     }
 }
--- src/test/resources/tests/CreditCard.feature
+++ src/test/resources/tests/CreditCard.feature
@@ -1,7 +1,7 @@
 Feature: Account Holder withdraws cash
 
   @afm
-  Scenario: exception occurs in api call
+  Scenario: Balance is adjusted
     Given the account balance is 100
     And the card is valid
     And the machine contains 100
@@ -9,3 +9,8 @@ Feature: Account Holder withdraws cash
     Then the ATM should dispense 20
     And the account balance should be 70
     And the card should be returned
+
+  @afm @tba
+  Scenario: Test that tag exclusions work
+    Given this scenario is tagged as @tba
+    Then it will never be called
